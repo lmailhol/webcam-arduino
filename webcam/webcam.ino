1015b3fb304b13fb3a506985be9975be81f26171
Luca Mailhol <luca.mailhol@gmail.com>
2013-06-12 10:52:11 +0200

    Update webcam.ino

--- webcam/webcam.ino
+++ webcam/webcam.ino
@@ -1,20 +1,27 @@
 #include <Servo.h>
 
+#define SPEED 3
+#define DIRECTION 12
+
 Servo myservo;
 Servo myservo2;
 
-int servoSpeed = 95; //vitesse de rotation du servomoteur continue
-int servoSpeed2 = 95; //vitesse de rotation du servomoteur continue
-String readSerial, servo1, servo2;
+int servoSpeed; //vitesse de rotation du servomoteur continue
+int servoSpeed2; //vitesse de rotation du servomoteur continue
+String readSerial, servo1, servo2, motor;
+
+int motorSpeed;
+int motorDirection;
 
 void setup() {
   Serial.begin(115200);
-  myservo.attach(8);
-  myservo2.attach(9);
-  pinMode(8, OUTPUT); //broche en sortie
-  pinMode(9, OUTPUT); //broche en sortie
-  myservo.write(servoSpeed); //position initiale
-  myservo2.write(servoSpeed2); //position initiale
+  pinMode(8, OUTPUT); //broche en sortie servo1
+  pinMode(9, OUTPUT); //broche en sortie servo2
+  
+  pinMode(DIRECTION, OUTPUT); //broche direction
+  pinMode(SPEED, OUTPUT); //broche vitesse
+  
+  motorSpeed = 125;  
 }
 
 void loop() {
@@ -28,23 +35,61 @@ void loop() {
   if (readSerial.length() >0) {
     Serial.println("Chaine : "); 
     Serial.println(readSerial); //xxxxxx
-    servo1 = readSerial.substring(0, 3); //get the first two characters
-    servo2 = readSerial.substring(3, 6); //get the next two characters     
+    servo1 = readSerial.substring(0, 2); //get the first two characters
+    servo2 = readSerial.substring(2, 4); //get the next two characters     
+    motor = readSerial.substring(4, 5);
     Serial.println("Servomoteur 1 : "); 
     Serial.println(servo1);
     Serial.println("Servomoteur 2 : ");
     Serial.println(servo2);
+    Serial.println("Motor Courant Continu : ");
+    Serial.println(motor);
     
-    char carray1[6]; //magic needed to convert string to a number 
+    char carray1[5]; //magic needed to convert string to a number 
     servo1.toCharArray(carray1, sizeof(carray1));
     servoSpeed = atoi(carray1); 
       
-    char carray2[6];
+    char carray2[5];
     servo2.toCharArray(carray2, sizeof(carray2));
     servoSpeed2 = atoi(carray2); 
-    //lancement de la fonction servoTurn
+    
+    char carray3[5];
+    motor.toCharArray(carray3, sizeof(carray3));
+    motorDirection = atoi(carray3); 
+    
+    myservo.attach(8);
+    myservo2.attach(9);
     myservo.write(servoSpeed);
     myservo2.write(servoSpeed2);
+    
+    analogWrite(SPEED, motorSpeed);
+    
+    if(motorDirection == 0) {
+      digitalWrite(DIRECTION,HIGH);
+    } else if(motorDirection == 1) {
+      digitalWrite(DIRECTION,LOW);
+    } else if(motorDirection == 2) {
+      analogWrite(SPEED, 0);
+    }
+
+    /*if (myservo.attached() && myservo2.attached()) {
+      if (servoSpeed != 90 || servoSpeed2 != 90) {
+        myservo.write(servoSpeed);
+        myservo2.write(servoSpeed2);
+      } else {
+        //myservo.detach();
+        //myservo2.detach();
+        myservo.write(90);
+        myservo2.write(90);
+      }
+    } else {
+      if (servoSpeed != 90 || servoSpeed2 != 90) {  
+        myservo.attach(8);
+        myservo2.attach(9);
+        myservo.write(servoSpeed);
+        myservo2.write(servoSpeed2);
+      }
+    }*/
     //remise a zero de la valeur de recuperation
     //servo1 = "";
     //servo2 = "";
